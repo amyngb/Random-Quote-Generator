a27a313cf7544796f43b7f7c060decd4c8e88112
Amy Bedinghaus <amyngbedinghaus@gmail.com>
2017-07-08 11:25:13 -0400

    finished...?

--- js/quotes.js
+++ js/quotes.js
@@ -8,48 +8,53 @@
 
 
  (function() {
-  //Create quotations with constructor function
-  var Quotation = function(quotation) {
-      this.quotation = quotation;
-  }
+    //Create quotations in an object
+    var quotation = {
+      q1:["Don\'t believe everything you think.",'unknown'],
+      q2:["You must do the thing you think you cannot do.",'Eleanor Roosevelt'],
+      q3:["Lorem ipsum.",'classical Latin literature.'],
+      q4: ["Work hard and say it\'s easy.",'Julian Casablancas'],
+      q5: ["If you can\'t explain it simply, you don\'t understand it well enough.",'Albert Einstein'],
+      q6: ["Silent gratitude isn\'t very much to anyone.",'Gertrude Stein'],
+      q7: ['Whether you think you can or think you can\’t, you\’re right.','Henry Ford'],
+      q8: ["Youth can not know how age thinks and feels, but old men are guilty if they forget what it is to be young.",'Albus Dumbledore']
+    };
 
-  var q1 = new Quotation ("Don't believe everything you think. -unknown");
-  var q2 = new Quotation ("You must do the thing you think you cannot do.  -Eleanor Roosevelt");
-  var q3 = new Quotation ('Lorem ipsum. -classical Latin literature.');
+      //store quotations in an array
+    console.log(quotation.q1[0]);
+    var quotations, ranNum, randomQ, attributedTo;
 
-//store quotations in an array
+    quotations = [quotation.q1[0], quotation.q2[0], quotation.q3[0], quotation.q4[0],
+    quotation.q5[0], quotation.q6[0], quotation.q7[0], quotation.q8[0]];
+    console.log(quotations);
 
-  var quotations, ranNum, randomQ;
+    attributedTo = [quotation.q1[1], quotation.q2[1], quotation.q3[1], quotation.q4[1],
+    quotation.q5[1], quotation.q6[1], quotation.q7[1], quotation.q8[1]]
+    // Set up event listener
+    var setupEventListeners = function() {
+      document.querySelector('#getMessage').addEventListener('click', addMessage);
+      document.querySelector('.btn').addEventListener('click', tweetMessage);
+    };
 
-  quotations = [q1, q2, q3];
+    var addMessage = function () {
+      //Select a random quotation
+      ranNum = Math.floor(Math.random() * quotations.length);
+      randomQ = quotations[ranNum];
+      randomA = attributedTo[ranNum];
+      //Add it to DOM
+      document.querySelector('.message').innerHTML = randomQ;
+      document.querySelector('.attributedTo').innerHTML = randomA;
+      };
 
+    var tweetMessage = function () {
+      var tweet = document.querySelector('.message').innerText + ' -' +
+        document.querySelector('.attributedTo').innerText;
+      console.log(tweet);
+      document.querySelector('.btn').href = 'http://twitter.com/home?status=' + tweet;
 
-  //Select a random quotation
-  ranNum = Math.floor(Math.random() * 3);
-  randomQ = quotations[ranNum].quotation;
-  console.log(randomQ);
+      };
 
-  // Set up event listeners
-  var setupEventListener = function() {
-
-    document.querySelector('#getMessage').addEventListener('click', ctrlAddMessage);
-
-  };
-
-  // Add quotation to DOM
-  var ctrlAddMessage = function() {
-    //1. get random quotation
-    var message = randomQ;
-    //2. add RQ to UI
-
-    
-    document.querySelector('.message').innerHTML = message;
-
-  };
-
-  //repeat on click
-
-  setupEventListener();
+    setupEventListeners();
 
 })();
 
